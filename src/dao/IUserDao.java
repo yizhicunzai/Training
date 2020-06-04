27923b702a2f9cdd159cef901b1e3348459f1a9a
yizhicunzai <as617471599@outlook.com>
2020-06-04 18:02:59 +0800

    ok

--- src/dao/IUserDao.java
+++ src/dao/IUserDao.java
@@ -6,11 +6,11 @@ import entity.User;
 
 public interface IUserDao {
 	public List queryUserAll();
-    //根据用户名查询所有数据，返回List
+
     public List queryAllByUname(String user_name);
-    //添加用户信息
+
     public int addUser(User user);
-    //修改用户信息
+
     public int updateUser(User user);
 	public List queryuser(String user_name, String user_password);
 	public int deleteUser(User user);
